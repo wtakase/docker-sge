0d385c86dea031aa26b6d04cb6cf844e49d859ce
Wataru Takase <wataru.takase@kek.jp>
2015-12-24 17:09:11 +0900

    Improve the way to install master and worker

--- master/run.sh
+++ master/run.sh
@@ -18,11 +18,9 @@ sed -e 's/^SGE_JMX_PORT=.*/SGE_JMX_PORT="6666"/' \
     -e 's/^SGE_JMX_SSL_KEYSTORE=.*/SGE_JMX_SSL_KEYSTORE="\/tmp"/' \
     -e 's/^SGE_JMX_SSL_KEYSTORE_PW=.*/SGE_JMX_SSL_KEYSTORE_PW="\/tmp"/' \
     -e 's/^SGE_JVM_LIB_PATH=.*/SGE_JVM_LIB_PATH="\/tmp"/' \
-    -e 's/^ADMIN_HOST_LIST=.*/ADMIN_HOST_LIST=\`hostname\`/' \
-    -e 's/^SUBMIT_HOST_LIST=.*/SUBMIT_HOST_LIST=\`hostname\`/' \
+    -e 's/^ADMIN_HOST_LIST=.*/ADMIN_HOST_LIST="${HOST_NAME:=${HOSTNAME}}"/' \
+    -e 's/^SUBMIT_HOST_LIST=.*/SUBMIT_HOST_LIST="${HOST_NAME:=${HOSTNAME}}"/' \
     -e 's/^EXEC_HOST_LIST=.*/EXEC_HOST_LIST=""/' \
     /opt/sge/util/install_modules/inst_template.conf > /opt/sge/install_sge_master.conf
-sed -e 's/^EXEC_HOST_LIST=.*/EXEC_HOST_LIST=\`hostname\`/' \
-    /opt/sge/install_sge_master.conf > /opt/sge/install_sge_worker.conf
 (cd /opt/sge; ./inst_sge -m -auto ./install_sge_master.conf)
 exec /usr/sbin/sshd -D
--- worker/run.sh
+++ worker/run.sh
@@ -5,5 +5,9 @@ mount -t nfs ${NFSHOME_PORT_2049_TCP_ADDR}:/ /home
 mount -t nfs ${NFSOPT_PORT_2049_TCP_ADDR}:/ /opt
 useradd -u 10000 sgeuser
 echo "sgeuser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-(sleep 10; sudo -u sgeuser bash -c "ssh ${SGEMASTER_PORT_22_TCP_ADDR} \"sudo bash -c '. /etc/profile.d/sge.sh; qconf -ah ${HOST_NAME:=${HOSTNAME}}; qconf -as ${HOST_NAME:=${HOSTNAME}}'\""; cd /opt/sge; ./inst_sge -x -auto ./install_sge_worker.conf -nobincheck -noremote) &
+sed -e 's/^ADMIN_HOST_LIST=.*/ADMIN_HOST_LIST="${HOST_NAME:=${HOSTNAME}}"/' \
+    -e 's/^SUBMIT_HOST_LIST=.*/SUBMIT_HOST_LIST="${HOST_NAME:=${HOSTNAME}}"/' \
+    -e 's/^EXEC_HOST_LIST=.*/EXEC_HOST_LIST="${HOST_NAME:=${HOSTNAME}}"/' \
+    /opt/sge/install_sge_master.conf > /tmp/install_sge_worker.conf
+(sleep 10; sudo -u sgeuser bash -c "ssh ${SGEMASTER_PORT_22_TCP_ADDR} \"sudo bash -c '. /etc/profile.d/sge.sh; qconf -ah ${HOST_NAME:=${HOSTNAME}}; qconf -as ${HOST_NAME:=${HOSTNAME}}'\""; cd /opt/sge; ./inst_sge -x -auto /tmp/install_sge_worker.conf -nobincheck -noremote) &
 exec /usr/sbin/sshd -D
