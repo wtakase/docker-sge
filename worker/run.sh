3273834be4fb5134e07ac168d88157e9c2ffb929
Wataru Takase <wataru.takase@kek.jp>
2015-12-28 09:37:07 +0900

    Remove noremote option from sge-worker

--- worker/run.sh
+++ worker/run.sh
@@ -9,5 +9,5 @@ sed -e 's/^ADMIN_HOST_LIST=.*/ADMIN_HOST_LIST="${HOST_NAME:=${HOSTNAME}}"/' \
     -e 's/^SUBMIT_HOST_LIST=.*/SUBMIT_HOST_LIST="${HOST_NAME:=${HOSTNAME}}"/' \
     -e 's/^EXEC_HOST_LIST=.*/EXEC_HOST_LIST="${HOST_NAME:=${HOSTNAME}}"/' \
     /opt/sge/install_sge_master.conf > /tmp/install_sge_worker.conf
-(sleep 10; sudo -u sgeuser bash -c "ssh ${SGEMASTER_PORT_22_TCP_ADDR} \"sudo bash -c '. /etc/profile.d/sge.sh; qconf -ah ${HOST_NAME:=${HOSTNAME}}; qconf -as ${HOST_NAME:=${HOSTNAME}}'\""; cd /opt/sge; ./inst_sge -x -auto /tmp/install_sge_worker.conf -nobincheck -noremote) &
+(sleep 10; sudo -u sgeuser bash -c "ssh ${SGEMASTER_PORT_22_TCP_ADDR} \"sudo bash -c '. /etc/profile.d/sge.sh; qconf -ah ${HOST_NAME:=${HOSTNAME}}; qconf -as ${HOST_NAME:=${HOSTNAME}}'\""; cd /opt/sge; ./inst_sge -x -auto /tmp/install_sge_worker.conf -nobincheck) &
 exec /usr/sbin/sshd -D
