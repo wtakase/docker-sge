e218d26924894993b3133231a26d53fc771cdb98
Wataru Takase <wataru.takase@kek.jp>
2015-12-28 17:58:20 +0900

    Change nsswitch.conf for checking DNS first

--- master/run.sh
+++ master/run.sh
@@ -18,9 +18,11 @@ sed -e 's/^SGE_JMX_PORT=.*/SGE_JMX_PORT="6666"/' \
     -e 's/^SGE_JMX_SSL_KEYSTORE=.*/SGE_JMX_SSL_KEYSTORE="\/tmp"/' \
     -e 's/^SGE_JMX_SSL_KEYSTORE_PW=.*/SGE_JMX_SSL_KEYSTORE_PW="\/tmp"/' \
     -e 's/^SGE_JVM_LIB_PATH=.*/SGE_JVM_LIB_PATH="\/tmp"/' \
-    -e 's/^ADMIN_HOST_LIST=.*/ADMIN_HOST_LIST="${HOST_NAME:=${HOSTNAME}}"/' \
-    -e 's/^SUBMIT_HOST_LIST=.*/SUBMIT_HOST_LIST="${HOST_NAME:=${HOSTNAME}}"/' \
+    -e 's/^ADMIN_HOST_LIST=.*/ADMIN_HOST_LIST=\`hostname -f\`/' \
+    -e 's/^SUBMIT_HOST_LIST=.*/SUBMIT_HOST_LIST=\`hostname -f\`/' \
     -e 's/^EXEC_HOST_LIST=.*/EXEC_HOST_LIST=""/' \
     /opt/sge/util/install_modules/inst_template.conf > /opt/sge/install_sge_master.conf
+sed -e 's/^EXEC_HOST_LIST=.*/EXEC_HOST_LIST=\`hostname -f\`/' \
+    /opt/sge/install_sge_master.conf > /opt/sge/install_sge_worker.conf
 (cd /opt/sge; ./inst_sge -m -auto ./install_sge_master.conf)
 exec /usr/sbin/sshd -D
